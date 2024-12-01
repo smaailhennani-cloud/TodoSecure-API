cebf064cff866e81fd5479930e4f5b7df51cd66e
smaailhennani-cloud <github34x@outlook.fr>
2024-12-01 16:52:49 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
+const fs = require('fs'); // Ajout pour lire le fichier SQL
 const app = express();
 
 process.on('uncaughtException', (err) => {
