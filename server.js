3adcb165a90cfc1e4093e0cc146938f6ac6d29f5
smaailhennani-cloud <github34x@outlook.fr>
2024-12-08 21:04:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -234,6 +234,7 @@ const handleDisconnect = () => {
         const { email, password } = req.body; // Ajouter des champs si nécessaire
         console.log("email, pswd : ", email, password);
         const hashedPassword = await bcrypt.hash(password, 10); // Hasher le mot de passe avec un salage de 10
+        console.log("New password hash add user : ",hashedPassword);
         db.query('INSERT INTO users (email, password) VALUES (?, ?)', [email, password], (err, results) => {
             if (err) return res.status(500).send(err);
             // Vérifiez que `results` contient `insertId`
