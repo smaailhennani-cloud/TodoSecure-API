ce627a07dffcf23550566190485d53b11443ed03
smaailhennani-cloud <github34x@outlook.fr>
2025-03-16 14:39:54 +0100

    feat(cors): mise à jour de la config CORS

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-// const userRoutes = require('./routes/users');
-// const todoRoutes = require('./routes/todos');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Configuration CORS
-app.use(cors({ origin: ['http://localhost:4200', 'https://gylgamesh34.github.io'] }));
+app.use(cors({ origin: ['http://localhost:4200', 'https://smaail-hennani.github.io'] }));
 
-app.use(express.json()); // Middleware pour le parsing JSON
+app.use(express.json());
 
-// Routes
 app.use('', routes);
-// app.use('', userRoutes);
-// app.use('/todos', todoRoutes);
 
 app.listen(PORT, () => console.log(`Serveur démarré sur http://localhost:${PORT}`));
