7ad72730d3ad9f5b5f3aaffb7aef299a1b93acb8
smaailhennani-cloud <github34x@outlook.fr>
2025-03-16 01:19:10 +0100

    Déclaration globale de dbConfig

--- database/db.js
+++ database/db.js
@@ -2,8 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const mysql = require('mysql2');
 
+let dbConfig;
+
 if (process.env.DATABASE_URL) {
-  const dbConfig = {
+  dbConfig = {
     uri: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: true },
     multipleStatements: true
